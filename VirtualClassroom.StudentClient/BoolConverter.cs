2efce878695f9393ed766df10ec125c7d13eecb6
George <bluef0rever@abv.bg>
2013-03-03 21:25:09 +0200

    Added images to display instead of true/false in datagrid.

---
+++ VirtualClassroom.StudentClient/BoolConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace VirtualClassroom.StudentClient
+{
+    class BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = "Images/False.png";
+
+            if((bool)value)
+            {
+                path = "Images/True.png";
+            }
+
+            return path;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
